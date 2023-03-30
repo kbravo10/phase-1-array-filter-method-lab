2bfc2dc22360ff17fbcc45c9f4a8fcaed1cee11b
Kevin Bravo <kbravo1024@gmail.com>
2023-03-29 19:46:22 -0700

    complete array-filter lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+function findMatching(driverArr, stringDrver){
+    const tempArr =[];
+    driverArr.filter(function (driver){
+        if(driver.toUpperCase() === stringDrver.toUpperCase()){
+            tempArr.push(driver)
+        }
+    })
+    return tempArr;
+}
+
+function fuzzyMatch(driverArr, stringLetter){
+    const temparr = [];
+    driverArr.filter(function(driver){
+        if(driver.includes(stringLetter) && driver[0] === stringLetter[0]){
+            temparr.push(driver)
+        }
+    })
+    return temparr;
+}
+
+function matchName(driverArr, stringName){
+    return driverArr.filter(function(driver){
+        if(driver["name"] === stringName){
+            return driver;
+        }
+    })
+}
